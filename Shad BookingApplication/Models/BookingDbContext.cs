8d4b72583c6022ddc7a285e35e81dd42c02f9ca8
Ahmad_Ali <Ahmad_Ali@AHMAD>
2017-07-17 03:08:11 -0700

    View making

---
+++ Shad BookingApplication/Models/BookingDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Shad_BookingApplication.Models
+{
+    public class BookingDbContext: DbContext
+    {
+        public DbSet<AspNetUser> Users { get; set; }
+    }
+}
