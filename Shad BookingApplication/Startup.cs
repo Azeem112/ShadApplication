279d4fa715144132a82b47300a36a8691a32fd7c
Azeem <Azeem@SPOTLIGHT>
2017-07-17 12:27:08 +0500

    first Structure of shad application

---
+++ Shad BookingApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shad_BookingApplication.Startup))]
+namespace Shad_BookingApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
